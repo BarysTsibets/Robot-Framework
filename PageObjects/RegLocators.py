4aa2ad3165d68068064471aae63d510b6e359347
Barys_Tsibets <molekyla_08@mail.ru>
2021-07-30 12:20:08 -0700

    Locators for Register Page created

---
+++ PageObjects/RegLocators.py
@@ -0,0 +1,13 @@
+# locators for Registration Page
+
+r_btn_gender_male = "css:#gender-male"
+txt_firstName = "id:FirstName"
+txt_lastname = "id:LastName"
+dropdown_day = "name:DateOfBirthDay"            # 0...31
+dropdown_month = "name:DateOfBirthMonth"        # value = 0..12 /  label =  January...December
+dropdown_year = "name:DateOfBirthYear"          # value = 1911..2050 /  label =  1911...2050
+txt_email = "id:Email"
+checkbox_newsletter = "id:Newsletter"           # checked by Default
+txt_password = "id:Password"
+txt_confirm_password = "id:ConfirmPassword"
+btn_register = "id:register-button"
